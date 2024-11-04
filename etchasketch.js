6ac5e2d1ae7f9c0fbc71953727d97c15eac5d5d9
caitnbake <caitlin.atterberry@gmail.com>
2024-11-03 17:43:56 -0800

    Add button to clear existing grid

--- etchasketch.js
+++ etchasketch.js
@@ -2,6 +2,7 @@ const NUM = 16;
 drawBoxes(NUM);
 
 const resetBtn = document.querySelector(".reset");
+const clearBtn = document.querySelector(".clear");
 
 resetBtn.addEventListener("click", () => {
     let numberOfBoxes = +prompt("How many boxes?");
@@ -11,11 +12,20 @@ resetBtn.addEventListener("click", () => {
     drawBoxes(numberOfBoxes);
 });
 
+clearBtn.addEventListener("click", clearGrid);
+
+function clearGrid() {
+    let existingBoxes = document.querySelectorAll(".newBox");
+    for (let box of existingBoxes) {
+        box.style.backgroundColor = "white";
+    }
+}
+
 function drawBoxes(numberOfBoxes = 16) {
     const boxWidth = ((1 / numberOfBoxes) * 100) + "%";
     const container = document.querySelector(".container");
 
-    // Clears existing boxes if there are any
+    // Deletes existing boxes if there are any
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     };
@@ -33,6 +43,6 @@ function drawBoxes(numberOfBoxes = 16) {
     
 function makeBoxesFillable(newBoxes) {
     for (let newBox of newBoxes) {
-        newBox.addEventListener("mouseenter", () => newBox.style.backgroundColor = "blue");
+        newBox.addEventListener("mouseenter", () => newBox.style.backgroundColor = "black");
     }
 }
