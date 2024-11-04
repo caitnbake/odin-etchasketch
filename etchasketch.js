d5ccd7108f6595b8c3db8947373894aa268fcae3
caitnbake <caitlin.atterberry@gmail.com>
2024-11-03 17:08:59 -0800

    Add reset button

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,20 @@
 const NUM = 16;
-const container = document.querySelector(".container");
-
-// Ensures the right number of rows/columns fill up the container
 const boxWidth = ((1 / NUM) * 100) + "%";
+
+const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", () => {
+    prompt("How many boxes?");
+    for (let newBox of newBoxes) {
+        newBox.style.backgroundColor = "white";
+    }
+});
 
 // Initialize boxes
 for (let i = 0; i < (NUM ** 2); i++) {
     const newBox = document.createElement("div");
     newBox.style.width = `${boxWidth}`;
     newBox.style.aspectRatio = `1 / 1`;
-    newBox.style.border = "1px solid black"
     newBox.classList.add("newBox");
     container.appendChild(newBox);
 }
